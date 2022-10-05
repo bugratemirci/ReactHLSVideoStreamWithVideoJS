d3b688d0d84ec49821e6c222f9fe6423f9e160b9
bugratemirci <bugrahanetemircii@gmail.com>
2022-10-05 23:08:56 +0300

    hls link delete

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', position: 'absolute', left: '0px', width: '100%', overflow: 'hidden' }}>
       <VideoJS
-        link={"https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"}
+        link={"HLS URL"}
       />
     </div>
   );
